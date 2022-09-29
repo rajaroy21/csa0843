c1ac19baf555469ec3c603f5a861184c4ed6ac95
rajaroy21 <112242469+rajaroy21@users.noreply.github.com>
2022-09-29 13:17:13 +0530

    Add files via upload

---
+++ square and cube of decimal num.py
@@ -0,0 +1,5 @@
+num = float(input("Enter any  decimal num : "))
+square=num * num
+print("The Square of a number {0} = {1}".format(num,square))
+cube = num * num * num
+print("The Cube of a number {0}  = {1}".format(num,cube))
