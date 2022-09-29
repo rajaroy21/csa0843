939d8f455eb53c8e8c4c5626bffd043ab359c6e7
rajaroy21 <112242469+rajaroy21@users.noreply.github.com>
2022-09-29 13:32:03 +0530

    Add files via upload

---
+++ vowels in string.py
@@ -0,0 +1,6 @@
+s=str(input("enter string :"))
+vowels=0
+for i in s:
+     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+         vowels=vowels+1
+print(vowels)
