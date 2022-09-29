95ef923deeb2526b53536dc79193f2bd6c45432b
rajaroy21 <112242469+rajaroy21@users.noreply.github.com>
2022-09-29 08:18:58 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,13 @@
+n=int(input("enter the value of n:"))
+m=int(input("enter the value of m:"))
+list1=[]
+list2=[]
+list3=[]
+for i in range(1,n+1):
+    b=int(input("enter the values in list1:"))
+    list1.append(b)
+for j in range(1,m+1):
+    c=int(input("enter the values of list2:"))
+    list2.append(c)
+list3=list1+list2
+print(list3)
